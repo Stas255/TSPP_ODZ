f5f93e0626d4a542349b77e543dcb2e6518780a9
Станіслав Толстоноженко <stastolsto@gmail.com>
2019-04-20 00:45:31 +0300

    1

---
+++ ODZ/BooksClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODZ
+{
+    public class BooksClass
+    {
+        public string IDBook { get; set; }
+        public string NameAvtor { get; set; }
+        public string Title { get; set; }
+        public int Date { get; set; }
+        public string Placing { get; set; }
+
+        public BooksClass(string IDBook, string NameAvtor, string Title, int Date, string Placing)
+        {
+            this.IDBook = IDBook;
+            this.NameAvtor = NameAvtor;
+            this.Title = Title;
+            this.Date = Date;
+            this.Placing = Placing;
+        }
+    }
+}
---
+++ ODZ/bin/Debug/libraryBooks.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Апр 19 2019 г., 23:15
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `library`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `books`
+--
+
+CREATE TABLE IF NOT EXISTS `books` (
+  `Шифр` int(5) NOT NULL,
+  `Прізвище` varchar(25) NOT NULL,
+  `Назва` varchar(25) NOT NULL,
+  `Рік видання` int(4) NOT NULL,
+  `Місце розташування` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
