aee563815f030082a6d848f3ee89f15bca6c9314
Станіслав Толстоноженко <stastolsto@gmail.com>
2019-04-20 02:58:07 +0300

    3

--- ODZ/MainWindow.xaml.cs
+++ ODZ/MainWindow.xaml.cs
@@ -14,10 +14,11 @@ namespace ODZ
         private string connStr;
         public List<BooksClass> fList = new List<BooksClass>();
         int bookNum;
+        bool bookAdd = false;
         public MainWindow()
         {
             InitializeComponent();
-            OpenDbFile();
+            
         }
 
         private void OpenDbFile()
@@ -86,6 +87,145 @@ namespace ODZ
                     "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             OpenDbFile();
+        }
+
+        private void EditDataMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            GroupBoxEdit.Visibility = Visibility.Visible;
+            this.Height = BooksListDG.Margin.Top + BooksListDG.RenderSize.Height + 50 +
+                          GroupBoxEdit.RenderSize.Height + 200;
+            Button1.Content = "Редагувати";
+        }
+
+        private void inforBookForm_Loaded(object sender, RoutedEventArgs e)
+        {
+            OpenDbFile();
+
+            this.Width = BooksListDG.Margin.Left + BooksListDG.RenderSize.Width + 50;
+            this.Height = BooksListDG.Margin.Top + BooksListDG.RenderSize.Height + 50;
+        }
+
+        private void AddDataMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //GroupBoxEdit.Visibility = Visibility.Visible;
+            this.Height = BooksListDG.Margin.Top + BooksListDG.RenderSize.Height + 50 +
+                          GroupBoxEdit.RenderSize.Height + 20;
+            Button1.Content = "Додати";
+            
+            bookNum = fList.Count;
+        }
+
+        private void ChangeFlightListData(int num)
+        {
+            TimeSpan depTime;
+            if (bookAdd)
+            {
+                fList.Add(new BooksClass("", "", "", 0, ""));
+            }
+
+            fList[num].Title = TextBoxTitle.Text;
+            fList[num].Date = Convert.ToInt16(TextBoxDate.Text);
+            fList[num].IDBook = TextBoxIDBook.Text;
+            fList[num].NameAuthor = TextBoxNameAuthor.Text;
+            fList[num].Placing = TextBoxPlacing.Text;
+
+
+
+            BooksListDG.ItemsSource = null;
+            BooksListDG.ItemsSource = fList;
+            if (bookAdd)
+            {
+                try
+                {
+                    using (MySqlConnection conn = new MySqlConnection(connStr))
+                    using (MySqlCommand cmd =
+                        new MySqlCommand(
+                            "INSERT INTO books (IDBook, NameAuthor, Title, Date, Placing) VALUES (?,?,?,?,?)",
+                            conn))
+                    {
+                        cmd.Parameters.Add("@idbook", MySqlDbType.VarChar, 5).Value = TextBoxIDBook.Text;
+                        cmd.Parameters.Add("@nameauthor", MySqlDbType.VarChar, 25).Value = TextBoxNameAuthor.Text;
+                        cmd.Parameters.Add("@title", MySqlDbType.VarChar, 25).Value = TextBoxTitle.Text;
+                        cmd.Parameters.Add("@date", MySqlDbType.Int16, 4).Value =
+                            Convert.ToInt16(TextBoxDate.Text);
+                        cmd.Parameters.Add("@placing", MySqlDbType.VarChar,7).Value = TextBoxPlacing.Text;
+                        cmd.Parameters.Add("@id", MySqlDbType.Int16, 11).Value = num + 1;
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string errMsg = "";
+                    if (ex.Message == "Unable to connect to any of the specified MySQL hosts.")
+                    {
+                        errMsg = "Підключення веб-сервер MySQL та завантажте дані командою Файл-Завантажити";
+                    }
+                    else
+                    {
+                        errMsg = "Для завантаження даних виконайте команду Файл-Завантаджити";
+                    }
+
+                    MessageBox.Show(ex.Message + char.ConvertFromUtf32(13) + char.ConvertFromUtf32(13) + errMsg,
+                        "Помика",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                try
+                {
+                    using (MySqlConnection conn = new MySqlConnection(connStr))
+                    using (MySqlCommand cmd =
+                        new MySqlCommand(
+                            "UPDATE books SET idbook = ?, nameauthor = ?, title = ?, date = ?, placing = ?  WHERE id = ?",
+                            conn))
+                    {
+                        cmd.Parameters.Add("@idbook", MySqlDbType.VarChar, 5).Value = TextBoxIDBook.Text;
+                        cmd.Parameters.Add("@nameauthor", MySqlDbType.VarChar, 25).Value = TextBoxNameAuthor.Text;
+                        cmd.Parameters.Add("@title", MySqlDbType.VarChar, 25).Value = TextBoxTitle.Text;
+                        cmd.Parameters.Add("@date", MySqlDbType.Int16, 4).Value =
+                            Convert.ToInt16(TextBoxDate.Text);
+                        cmd.Parameters.Add("@placing", MySqlDbType.VarChar, 7).Value = TextBoxPlacing.Text;
+                        cmd.Parameters.Add("@id", MySqlDbType.Int16, 11).Value = num + 1;
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string errMsg = "";
+                    if (ex.Message == "Unable to connect to any of the specified MySQL hosts.")
+                    {
+                        errMsg = "Підключення веб-сервер MySQL та завантажте дані командою Файл-Завантажити";
+                    }
+                    else
+                    {
+                        errMsg = "Для завантаження даних виконайте команду Файл-Завантаджити";
+                    }
+
+                    MessageBox.Show(ex.Message + char.ConvertFromUtf32(13) + char.ConvertFromUtf32(13) + errMsg,
+                        "Помика",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+        }
+
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (Button1.Content == "Додати")
+            {
+                bookAdd = true;
+                ChangeFlightListData(bookNum);
+                LoadDataMenuItem_Click(sender, e);
+            }
+            else
+            {
+                bookAdd = false;
+                ChangeFlightListData(bookNum);
+            }
         }
     }
 }
